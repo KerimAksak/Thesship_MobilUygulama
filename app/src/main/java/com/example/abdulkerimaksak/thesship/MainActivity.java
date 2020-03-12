5c96a14c38a53fb415f8f8e9b2f7cc94d984233c
kerimaksak <abdulkerim1783@hotmail.com>
2020-03-13 00:41:50 +0300

    Karşılama(MainActivity) fonksiyonları tamamlandı. Geçiş animasyonu eklendi.

--- app/src/main/java/com/example/abdulkerimaksak/thesship/MainActivity.java
+++ app/src/main/java/com/example/abdulkerimaksak/thesship/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.abdulkerimaksak.thesship;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_SIGNUP = 0;
+    Button btnGirisYap;
+    TextView tvKayitOl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialWork();
+        exqListener();
+
     }
+
+    private void initialWork() {
+        btnGirisYap = (Button) findViewById(R.id.btn_girisYap);
+        tvKayitOl = (TextView) findViewById(R.id.tv_kayitOl);
+    }
+
+    private void exqListener() {
+
+        btnGirisYap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);//Sağa geçiş animasyonu.
+            }
+        });
+
+        tvKayitOl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);//Sağa geçiş animasyonu.
+            }
+        });
+
+    }
+
 }
